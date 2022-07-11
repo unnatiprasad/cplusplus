ae883eec88991c9d1682f5b232a191dbaff599f3
unnatiprasad <83784798+unnatiprasad@users.noreply.github.com>
2022-07-11 21:50:01 +0530

    Update lightsout.cpp

--- lightsout.cpp
+++ lightsout.cpp
@@ -2,60 +2,12 @@
 using namespace std;
 int main()
 {
-	int arr[3][3];
-	for(int i=0;i<3;i++)
-	{
-		for(int j=0;j<3;j++)
-		{
-			cin>>arr[i][j];
-		}
-	}
-	int sw[3][3];
-	for(int i=0;i<3;i++)
-	{
-	for(int j=0;j<3;j++)
-	{
-		int sum_hori=0;
-		int sum_verti=0;
-		for(int row=0;row<3;row++)
-		{
-			if(row!=i)
-			{
-				sum_verti+=arr[row][j];
-			}
-		}
-		for(int col=0;col<3;col++)
-		{
-			if(col!=j)
-			{
-				sum_hori+=arr[i][col];
-			}
-		}
-		sw[i][j]=arr[i][j]+sum_hori+sum_verti;
-		}
-		
-	}
-	for(int i=0;i<3;i++)
-	{
-		for(int j=0;j<3;j++)
-		{
-			if(sw[i][j]%2!=0)
-			{
-				sw[i][j]=0;
-			}
-			else if(sw[i][j]%2==0)
-			{
-				sw[i][j]=1;
-			}
-		}
-	}
-	for(int i=0;i<3;i++)
-	{
-		for(int j=0;j<3;j++)
-		{
-			cout<<sw[i][j];
-		}
-		cout<<endl;
-	}
-	
+    int x11, x12, x13, x21, x22, x23, x31, x32, x33;
+    cin >> x11 >> x12 >> x13 >> x21 >> x22 >> x23 >> x31 >> x32 >> x33;
+    cout << ((x11 + x12 + x21) % 2 == 0 ? "1" : "0") << ((x11 + x12 + x13 + x22) % 2 == 0 ? "1" : "0") << ((x12 + x13 + x23) % 2 == 0 ? "1" : "0") << endl;
+    cout << ((x11 + x21 + x22 + x31) % 2 == 0 ? "1" : "0") << ((x12 + x21 + x22 + x23 + x32) % 2 == 0 ? "1" : "0") << ((x13 + x22 + x23 + x33) % 2 == 0 ? "1" : "0") << endl;
+    cout << ((x21 + x31 + x32) % 2 == 0 ? "1" : "0") << ((x22 + x31 + x32 + x33) % 2 == 0 ? "1" : "0") << ((x23 + x32 + x33) % 2 == 0 ? "1" : "0") << endl;
+    return 0;
 }
+
+	
